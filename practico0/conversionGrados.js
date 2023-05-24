efa71e3b68de173a4154027287044365685f1e92
slucero1985 <sebastian.matias.lucero@gmail.com>
2023-05-23 22:12:04 -0300

    Agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,3 +1,7 @@
-let grados_fahrenheit = 50
-let grados_celsius = (grados_fahrenheit - 32) * (5/9)
-console.log(grados_celsius)
+let gradosFahrenheit = 50;
+let gradosCelsius = (gradosFahrenheit - 32) * (5/9);
+console.log(gradosCelsius);
+
+let gradosFahrenheit1 = 75;
+let gradosCelsius1 = (gradosFahrenheit1 - 32) * (5/9);
+console.log(gradosCelsius1);
