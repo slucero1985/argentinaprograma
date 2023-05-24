9dc12d5d7923970351086abe4c3f6b9f58d6b12c
slucero1985 <sebastian.matias.lucero@gmail.com>
2023-05-23 22:13:00 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -1,4 +1,9 @@
-let base = 5
-let altura = 3
+let base = 5;
+let altura = 3;
 let resultado = base * altura;
 console.log(resultado);
+
+let base1 = 12.5;
+let altura1 = 5.3;
+let resultado1 = base1 * altura1;
+console.log(resultado1);
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,5 +1,17 @@
 let numero1 = 5;
 let numero2 = 7;
 let numero3 = 9;
-let promedio = (numero1 + numero2 + numero3) / 3
+let promedio = (numero1 + numero2 + numero3) / 3;
 console.log(promedio);
+
+let numero1a = -2;
+let numero2a = -3;
+let numero3a = -8;
+let promedioA = (numero1a + numero2a + numero3a) / 3;
+console.log(promedioA);
+
+let numero1b = -1.2;
+let numero2b = 5.3;
+let numero3b = -3.3;
+let promedioB = (numero1b + numero2b + numero3b) / 3;
+console.log(promedioB);
